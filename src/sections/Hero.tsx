93a76145aa292fa38d1443d001b5998b85643a61
Joshuaosunlakin3433 <64253576+Joshuaosunlakin3433@users.noreply.github.com>
2025-07-04 00:29:57 +0100

    feat: Add Footer component with social links and styling; enhance Hero section with animations

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,20 +1,38 @@
+"use client";
 import ArrowRight from "@/assets/arrow-right.svg";
 import cogImage from "@/assets/cog.png";
 import Image from "next/image";
 import cylinderImage from "@/assets/cylinder.png";
-import noodleImage from "@/assets/noodle.png"
+import noodleImage from "@/assets/noodle.png";
+import {
+  motion,
+  useScroll,
+  useTransform,
+  useMotionValueEvent,
+} from "framer-motion";
+import { useRef } from "react";
+import { Alegreya } from "next/font/google";
+
 export const Hero = () => {
+  const heroRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: heroRef,
+    offset: ["start end", "end start"],
+  });
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+  // useMotionValueEvent(translateY, "change", (latestValue) =>
+  //   console.log(latestValue)
+  // );
   return (
-    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip">
+    <section
+      ref={heroRef}
+      className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip"
+    >
       <div className="container ">
         <div className="md:flex items-center">
           <div className="md:w-[478px]">
-            <div className="tag">
-              Version 2.0 is here
-            </div>
-            <h1 className="header">
-              Pathway to productivity
-            </h1>
+            <div className="tag">Version 2.0 is here</div>
+            <h1 className="header">Pathway to productivity</h1>
             <p className="text-xl ">
               Celebrate the joy of accomplishment with an app designed to track
               your progress, motivate your efforts, and celebrate your successes{" "}
@@ -29,13 +47,38 @@ export const Hero = () => {
           </div>
 
           <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
-            <Image
-              src={cogImage}
+            <motion.img
+              src={cogImage.src}
               alt="Cog image"
               className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0"
+              animate={{
+                translateY: [-30, 30],
+              }}
+              transition={{
+                repeat: Infinity,
+                repeatType: "mirror",
+                duration: 3,
+                ease: "easeInOut",
+              }}
             />
-            <Image src={cylinderImage} alt="Cylinder Image" height={220} className="hidden md:block -top-8 -left-32 absolute" />
-            <Image src={noodleImage} alt="Cylinder Image" width={220} className="hidden lg:block top-[524px] left-[448px] rotate-[30deg] absolute" />
+            <motion.img
+              src={cylinderImage.src}
+              alt="Cylinder Image"
+              height={220}
+              className="hidden md:block -top-8 -left-32 absolute"
+              style={{
+                translateY: translateY,
+              }}
+            />
+            <motion.img
+              src={noodleImage.src}
+              alt="Cylinder Image"
+              width={220}
+              className="hidden lg:block top-[524px] left-[448px] rotate-[30deg] absolute"
+               style={{
+                translateY: translateY,
+              }}
+            />
           </div>
         </div>
       </div>
